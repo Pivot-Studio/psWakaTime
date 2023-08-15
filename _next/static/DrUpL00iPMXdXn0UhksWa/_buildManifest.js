52a806f37ef61211d2b65da1cc463bd9df897ec5
husterdjx <husterdjx@users.noreply.github.com>
2023-08-15 03:02:56 +0000

    deploy: 690364c005675f753b0ff1c677169ab2dabc4607

--- _next/static/DrUpL00iPMXdXn0UhksWa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-d72d9e1cf6dc4f27.js","static/css/0956c974e0866741.css","static/chunks/pages/index-d854063a6b5cc3a8.js"],"/_error":["static/chunks/pages/_error-0dc995e682681369.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
