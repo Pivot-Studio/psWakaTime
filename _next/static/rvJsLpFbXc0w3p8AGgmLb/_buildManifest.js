13fe2b214f1dd1878a85c39d30dd0633442a291d
Chronostasys <Chronostasys@users.noreply.github.com>
2023-08-15 03:04:27 +0000

    deploy: c1a42b827d0fa5722ddb68e74bfcae61c48c5a83

---
+++ _next/static/rvJsLpFbXc0w3p8AGgmLb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-00eb9ebede35e551.js","static/css/70ac8f905c7f784f.css","static/chunks/pages/index-e1eeb15652ac41be.js"],"/_error":["static/chunks/pages/_error-0dc995e682681369.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
