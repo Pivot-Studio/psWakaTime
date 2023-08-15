7ed6ddd64e1174e9dc4e74198747d3c66a114c8f
xieyuschen <xieyuschen@users.noreply.github.com>
2023-08-15 02:56:04 +0000

    deploy: 03365ce34763a6ceb543b30fa40e40573e16c93a

--- _next/static/Jil3d99SmXf6vRZqvS-9P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-d72d9e1cf6dc4f27.js","static/css/0956c974e0866741.css","static/chunks/pages/index-6f4e0d8ad3bffc5a.js"],"/_error":["static/chunks/pages/_error-0dc995e682681369.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
