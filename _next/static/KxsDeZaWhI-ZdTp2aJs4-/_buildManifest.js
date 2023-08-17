068893ab06e3f6f1a590263b4b75ab490e3f9619
Chronostasys <Chronostasys@users.noreply.github.com>
2023-08-17 01:28:48 +0000

    deploy: 52369aa5562a8bb68e6acbd3543c609baaf8d02e

--- _next/static/KxsDeZaWhI-ZdTp2aJs4-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-308805aa603ca0cf.js","static/css/a9e2c45b56cf9194.css","static/chunks/pages/index-bc244c0558cb4b73.js"],"/_error":["static/chunks/pages/_error-0dc995e682681369.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/P7W5zJt1H4QVVusPQXDZO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-308805aa603ca0cf.js","static/css/a9e2c45b56cf9194.css","static/chunks/pages/index-a27c27a4690a5f86.js"],"/_error":["static/chunks/pages/_error-0dc995e682681369.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
