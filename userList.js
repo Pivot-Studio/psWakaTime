52369aa5562a8bb68e6acbd3543c609baaf8d02e
Boxiu Lee <49905799+Chronostasys@users.noreply.github.com>
2023-08-17 09:27:16 +0800

    Update userList.js

--- userList.js
+++ userList.js
@@ -1,4 +1,4 @@
 // 先把地区改到新加坡，然后填入自己在设置界面填入的的full name即可(如果只填userName不填fullName，那就填入@ + 自己的userName)
 export const userList = [
-  'matto','@NolanHo','smith','MoonOldh', '@griffinx', "Diagonal", 'CjiW','A1eX',"@7erry","mama"
+  'matto','@NolanHo','smith','MoonOldh', 'Griffinx', "Diagonal", 'CjiW','A1eX',"@7erry","mama"
 ]
